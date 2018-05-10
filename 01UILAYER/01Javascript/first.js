85f188489ccd5465cdf189034176dbe2605db6e7
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2018-05-10 15:16:24 +0530

    js basics

---
+++ 01UILAYER/01Javascript/first.js
@@ -0,0 +1,111 @@
+console.log('Any message')
+
+// Function types
+// Numbers, Boolean, Strings
+
+// Functions as objects
+
+// Function as arguments
+
+var sum = function(x, y) { 
+    return x + y
+}
+
+function abc(fn, a, b){
+    fn(a,b)
+}
+abc(sum, 1,2)
+  
+
+
+// Returning function from a function
+function outer(){
+	console.log('outer function')
+
+	var inner = function(){
+		console.log('inner function')
+    }
+	return inner;
+
+}
+
+// Function callbacks
+
+console.log('Starting')
+setTimeout( function(){
+    console.log('foo')
+}, 5000)
+
+console.log('End')
+
+
+// SYNC FLOW
+function thirdPartyFunction(){
+    console.log('Does some processing')
+    return "SUCCESS"
+}
+
+console.log('Starting')
+console.log(thirdPartyFunction())
+console.log('End')
+
+
+// ASYNC FLOW  - PROBLEM
+function thirdPartyFunction(){
+    
+    setTimeout( function(){
+        console.log('Does some processing');
+        return "SUCCESS"
+    }, 2000);
+   
+}
+
+console.log('Starting')
+console.log(thirdPartyFunction())
+console.log('End')
+
+
+
+// ASYNC FLOW  - PROBLEM
+function thirdPartyFunction(callback){
+    
+    setTimeout( function(){
+        console.log('Does some processing');
+        callback("SUCCESS") 
+    }, 2000);
+   
+}
+
+console.log('Starting')
+console.log(thirdPartyFunction(function(result){
+    console.log(result)
+}))
+console.log('End')
+
+
+// Promise as a solution to callback
+
+function thirdPartyFunction(){
+    return new Promise((resolve, reject) =>
+    setTimeout( function(){
+        console.log('Does some processing');
+        resolve("SUCCESS") 
+    }, 2000))
+   
+}
+
+console.log('Starting')
+thirdPartyFunction().then(function(result){
+    console.log(result)
+})
+console.log('End')
+
+
+$.get( "test.php" ).then(
+    function() {
+      alert( "$.get succeeded" );
+    }, function() {
+      alert( "$.get failed!" );
+    }
+  );
+  
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # fsdbatch_java
+
+    UI
+        HTML (Structure)
+            - Websocket
+            - Fetch API
+            - Local storage
+            - App Cache
+        CSS (Presentation)
+            - CSS 3 Transitions
+            - SVG
+        JS  (Behavior)
+            - Data types
+                Loosely typed language
+                Functions are treated as objects
+                Functions can be passed as arguments
+                Function can be retured from within a function
+                Function scopes
+
+            Ist Phase - Create scope
+                Outer Fn: [x: undefined , inner: undefined]
+                Inner Fn: [x: undefined]
+
+            IInd Phase - Execution
+                Outer Fn: [x: 2 , inner: fn]
+                Inner Fn: [x: undefined]
+            
+            - Async flow using setTimeout
+            - Callbacks in Javascript
+            - Callbacks vs Promises - Promises have come up as substitute to Callbacks, you dont have to expose functionality
+            http://es6-features.org/#PromiseUsage
+            - ECMA Script (ES) 
+                ES5, ES6, ES7
+
+
+        JQuery
+
+        Angular 5
+            Typescript
