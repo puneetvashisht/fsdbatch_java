1fb5743739e8d95127718c93d5ccb9dd782b554e
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2018-08-09 15:11:32 +0530

    git caching

---
+++ 02BUSINESSLAYER/gs-caching1/complete/src/main/java/hello/SimpleBookRepository.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleBookRepository implements BookRepository {
+
+    @Override
+    @Cacheable(value="books")
+    public Book getByIsbn(String isbn) {
+        simulateSlowService();
+        return new Book(isbn, "Some book");
+    }
+
+    // Don't do this at home
+    private void simulateSlowService() {
+        try {
+            long time = 3000L;
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
